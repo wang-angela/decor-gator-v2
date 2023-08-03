99db9e02acf2625d783ea435a015e701a28eab53
Angela <92885742+wang-angela@users.noreply.github.com>
2023-08-03 10:51:23 -0400

    Create basic footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <>
-      <div className="text-center p-3 bg-dark text-white">
+      <footer className="text-center p-3 bg-dark text-white">
         © 2023 DecorGator
-      </div>
+      </footer>
     </>
   );
 }
